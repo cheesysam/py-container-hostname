0f24ed30ab9a879fa8db7e654ad068d2164cada6
Sam Carter <sam@growkudos.com>
2018-03-31 19:18:03 +0100

    fix visits and redis remains

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello():
 
     html = "<h3>Hostname Printer</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
